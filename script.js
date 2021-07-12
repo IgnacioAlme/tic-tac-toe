654375562eb0d66687f66f17cd036233402a435c
IgnacioAlme <ignacio.yair.almenar@gmail.com>
2021-07-12 16:45:25 -0300

    Make basic game logic and minor html changes

--- script.js
+++ script.js
@@ -1,27 +1,122 @@
 const gameBoard = (() => {
-    let gameBoard = [];
+    let _gameBoard = new Array(9);
+
+    const playerFactory = (name, mark, ai, turn) => {
+        return {name, mark, ai, turn};
+    };
+    const player1 = playerFactory('player 1', 'X', false, True);
+    const player2 = playerFactory('Player 2', 'O', false, false)
+    let winner = null;
+    let turns = 0;
+
+    let board = []
+
+    const winCombos = [
+        [0,1,2],
+        [0,3,6],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,4,6],
+        [2,5,8],
+        [0,4,8]
+    ];
+    let winnerCombo = [];
+
+    const playerTurn = (function () {
+        const box = document.querySelectorAll('.box');
+        box.forEach( box => {
+            box.addEventListener('click', e => {
+                //X player move if conditions are met
+                if (player1.turn == true && gameBoard.winner == null 
+                    && e.target.textContent == '' && player2.ai == false) {
+                        //Adds move to array
+                        board[e.target.id] = player1.mark;
+
+                        box.textContent = player1.mark;
+                        box.style.color = '#EE6C4D';
+
+                        player1.turn = false;
+                        player2.turn = true;
+
+                        console.log(board);
+                    } else if (player2.turn == true && gameBoard.winner == null
+                        && e.target.textContent == '' && player2.ai == false) {
+                            board[e.target.id] = player2.mark;
+
+                            box.textContent = player2.mark;
+                            box.style.color = '#98C1D9';
+
+                            player1.turn = true;
+                            player2.turn = false;
+
+                            console.log(board);
+                        } else {
+                            return;
+                        };
+
+                        winCheck();
+
+                        //box.style.opacity = '1';
+            });
+        });
+        return {box};
+    })();
+
+    winCheck = () => {
+        turns++;
+
+        //Separates each player X | O move into 2 different arrays
+        let xPlays = board.reduce((a, e, i) =>
+        (e === player1.mark) ? a.concat(i) : a, []);
+        let oPlays = board.reduce((a, e, i) =>
+        (e === player2.mark) ? a.concat(i) : a, []);
+
+        //Loop iterates over each winCombo array
+        for(let [index, combo] of winCombos.entries()) {
+            //Check if player moves index is equal to combo array index
+            if (combo.every(elem => xPlays.indexOf(elem) > -1)) {
+                gameBoard.winner = 'player1';
+                gameBoard.winnerCombo = combo;
+            } else if (combo.every(elem => oPlays.indexOf(elem) > -1)) {
+                gameBoard.winner = 'player2';
+                gameBoard.winnerCombo = combo;
+            } else if (gameBoard.winner == null && gameBoard.winner == undefined && turns == 9) {
+                gameBoard.winner = 'tie';
+                gameBoard.winnerCombo = combo;
+            };
+        };
+        //Display the winner
+        console.log(turns, gameBoard.winner, winnerCombo);
+        winDisplay();
+        return winnerCombo;
+    };
+    //Resets board and display
+    gameReset = () => {
+
+    };
+    console.log(board, winner, player1.turn, player2.turn);
+    return {winCheck, gameReset, playerTurn, board, player2, winnerCombo};
 })();
 
-const playerFactory = (name, symbol) => {
-    let score = 0;
-    const getName = () => console.log({name});
-    const getScore = () => console.log(score);
-    return {getName, getScore};
-};
 
-const gameLogic = (() => {
-    const createPlayer = () => {
-        player1Name = prompt('Player one name', 'Player 1');
-        const player1 = playerFactory(player1Name, 'X');
-        console.log(`Player one created: ${player1Name}`)
+const displayController = (() => {
+    const boxCtn = document.querySelector('.box-ctn');
+    const box = document.querySelectorAll('.box');
+    const winCtn = document.querySelector('.win-display');
 
-        player2Name = prompt('Player two name', 'Player 2');
-        const player2 = playerFactory(player2Name, 'O');
-        console.log(`Player two created: ${player2Name}`)
-
-        return {player1, player2};
+    //Display winner function
+    windDisplay = () => {
+        for(i = 0; i < gameBoard.winnerCombo.length; i++) {
+            document.getElementById(gameBoard.winnerCombo[i]).style.
+            backgroundColor = 'rgba(0, 0, 0, 0.040)';
+        };
+    };
+    //Displays the winner
+    if(gameBoard.winner === 'p1') {
+        
     }
-    return {createPlayer};
 })();
 
-gameLogic.createPlayer();
+
+
